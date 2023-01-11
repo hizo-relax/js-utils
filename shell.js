1b3c818fb77190c4627bd2a2de70ea8a31577479
zouhai <zouhai@stary.com>
2023-01-11 17:47:15 +0800

    fix: shell

--- shell.js
+++ shell.js
@@ -1,16 +1,15 @@
-let shell = require('shelljs')
-let name = process.argv[2] || 'Auto-commit';
-let exec = shell.exec
+const shell = require('shelljs');
+const name = process.argv[2] || 'Auto-commit';
 
-if (exec('git add .').code !== 0) {
-    echo('Error: Git add failed')
-    exit(1)
+if (shell.exec('git add .').code !== 0) {
+    echo('Error: Git add failed');
+    exit(1);
 }
-if (exec(`git commit -am "${name}"`).code !== 0) {
-    echo('Error: Git commit failed')
-    exit(1)
+if (shell.exec(`git commit -am "${name}"`).code !== 0) {
+    echo('Error: Git commit failed');
+    exit(1);
 }
-if (exec('git push').code !== 0) {
-    echo('Error: Git commit failed')
-    exit(1)
+if (shell.exec('git push').code !== 0) {
+    echo('Error: Git push failed');
+    exit(1);
 }
